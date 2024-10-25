d2202c51722b709f0e6ba3ef49c4c4bcee198645
RBing123 <ryan3552215@gmail.com>
2024-10-25 10:16:27 +0800

    add read_matrix

---
+++ tests/assembly/read_matrix.s
@@ -0,0 +1,42 @@
+.golbl read_matrix
+.text
+# ==============================================================================
+# Arguments:
+#   a0 (char*) pointer to input filename
+#   a1 (int*) pointer to destination matrix
+#   a2 (int) number of elements to read
+# ==============================================================================
+read_matrix:
+    # Prologue
+    addi sp, sp, -32
+    sw ra, 28(sp)
+    sw s0, 24(sp)
+    sw s1, 20(sp)
+    sw s2, 16(sp)
+
+    # save arguments
+    mv s0, a0      # s0 = filename
+    mv s1, a1      # s1 = matrix pointer
+    mv s2, a2      # s2 = number of elements
+
+    # open file
+    li a1, 0       # read mode = 0
+    li a7, 1024    # syscall: open file
+    ecall
+
+    # error check
+    bltz a0, read_error
+    mv t0, a0      # data descripter
+
+    # read matrix data
+    mv a0, t0      # data descripter
+    mv a1, s1      # buffer address
+    li a2, 4       # 4 bytes per integer
+    mul a2, a2, s2 # total bytes needs to store
+    li a7, 63      # syscall: read file
+    ecall
+
+    # close file
+    mv a0, t0
+    li a7, 57      # syscall: close file
+    ecall
