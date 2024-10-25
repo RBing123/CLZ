80d9248af4cd9dff923d908ffbfd2f8456191505
RBing123 <ryan3552215@gmail.com>
2024-10-25 10:11:08 +0800

    add test_dot

---
+++ tests/assembly/test_dot.s
@@ -0,0 +1,45 @@
+.data
+    vector1: .word 1, 2, 3
+    vector2: .word 1, 3, 5
+    size:    .word 3
+    result_msg: .string "The dot product is: "
+    newline:    .string "\n"
+
+.global main
+
+main:
+    # Prologue
+    addi sp, sp, -8
+    sd ra, 0(sp)
+
+    # Arguments
+    la a0, vector1     # a0 = vector1 address
+    la a1, vector2     # a1 = vector2 address
+    lw a2, size        # a2 = size
+
+    # invoke dot.s
+    jal ra, dot        # a0 = save result
+
+    # save result
+    mv t0, a0
+
+    # print out message
+    li a7, 4           
+    la a0, result_msg
+    ecall
+
+    # print out result
+    li a7, 1           
+    mv a0, t0          
+    ecall
+
+    # print out newline
+    li a7, 4
+    la a0, newline
+    ecall
+
+    # Epilogue
+    ld ra, 0(sp)
+    addi sp, sp, 8
+             
+    ret
