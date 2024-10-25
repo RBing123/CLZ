7edfd1fffd000726f34204ecbe67c069b5ed5618
RBing123 <ryan3552215@gmail.com>
2024-10-25 09:21:20 +0800

    init dot.s

---
+++ tests/assembly/dot.s
@@ -0,0 +1,10 @@
+.globl dot
+
+.text
+# ==============================================================
+# Arguments:
+# a0: the address of vector 1
+# a1: the address of vector 2
+# Return:
+# a0: return the result of vector 1 inner product with vector 2
+# ==============================================================
