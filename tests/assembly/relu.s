ac4fe6f47cb8375c6e7f20e18dd511d0c08f7b7c
RBing123 <ryan3552215@gmail.com>
2024-10-29 22:19:38 +0800

    modify abstract

--- tests/assembly/relu.s
+++ tests/assembly/relu.s
@@ -2,7 +2,6 @@
 
 .text
 # ==============================================================================
-# FUNCTION: Performs an inplace element-wise ReLU on an array of ints
 # Arguments:
 # 	a0 is the pointer to the array
 #	a1 is the # of elements in the array
